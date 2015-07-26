dc11ed593ca62da0df658dba0f329e61f080f4f6
ababup1192 <ababup1192@gmail.com>
2015-07-26 13:01:19 +0900

    Implements ExecutorService and Par class

--- src/main/scala/org/ababup1192/par/Par.scala
+++ src/main/scala/org/ababup1192/par/Par.scala
@@ -1,6 +1,55 @@
 package org.ababup1192.par
 
+import java.util.concurrent.TimeUnit
+
+class ExecutorService {
+  def submit[A](a: Callable[A]): Future[A]
+}
+
+trait Callable[A] {
+  def call: A
+}
+
+trait Future[A] {
+  def get: A
+
+  def get(timeout: Long, unit: TimeUnit): A
+
+  def cancel(eventIfRunning: Boolean): Boolean
+
+  def isDone: Boolean
+
+  def isCancelled: Boolean
+}
+
 object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit) = get
+
+    def isCancelled = false
+
+    def cancel(eventIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      override def call: A = a(es).get
+    })
 
   def sum(ints: IndexedSeq[Int]): Int = {
     if (ints.size <= 1) {
